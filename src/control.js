6d4df928b7a09e7c4064cd725208cc0b402be686
60rzvvbj <1808078515@qq.com>
2022-01-23 12:27:41 +0800

    update control.js

--- src/control.js
+++ src/control.js
@@ -1,7 +1,7 @@
 import child_process from 'child_process';
 let exec = child_process.exec;
 function open() {
-	exec('node main.js', function (err, stdout, stderr) {
+	exec('node src/main.js', function (err, stdout, stderr) {
 		if (err) {
 			console.log(stdout);
 			console.log('==============');
